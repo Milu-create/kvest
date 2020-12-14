76f4e464ee916944a48e6f8d29c9c5b54f91c324
vadimkocetok <koshetok.com@mail.ru>
2020-12-14 11:17:59 +0300

    Исправила синтаксические ошибки

--- src/com/company/Story.java
+++ src/com/company/Story.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Story extends Еpisode {
     public Еpisode now_episode;
-    final Еpisode start = new Еpisode("Первый шаг к популярности",
+    public final Еpisode start = new Еpisode("Первый шаг к популярности",
             """
                     Вы вспомнили про свой старый канал на YuTube.Что вы сделаете?
                     (1)Вы купите новую камеру и микофон и начнёте снимать новые видео.
@@ -10,13 +10,13 @@ public class Story extends Еpisode {
                     (3)Будите снимать видео на старую камеру""", 3);
 
     public Story() {
-        start.many_situation[0] = new Еpisode("Успех!\n",
-                "Выпотратили много денег, но ваш канал вырос в 5 раз и заработали столько же, сколько потратили.",
+        start.many_situation[0] = new Еpisode("Успех!",
+                "Вы потратили много денег, но ваш канал вырос в 5 раз и заработали столько же, сколько вы потратили.",
                 4000, 0, 20);
-        start.many_situation[2] = new Еpisode("Большой успех!\n",
+        start.many_situation[2] = new Еpisode("Большой успех!",
                 "Вы не опустили руки ваш канал вырос в 5 раз благодаря вашему усердию.",
                 4000, 20000, 100);
-        start.many_situation[1] = new Еpisode("Вы упустили свою популярность.\n",
+        start.many_situation[1] = new Еpisode("Вы упустили свою популярность.",
                 "Вам нужно искать другие способы заработка.", -1000, 0, -2);
         now_episode = start;
     }
